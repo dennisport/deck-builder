2d3ba249be58a2a11ed5b8251f79a65f6c76f507
Dennis <Dennis.portillo@yale.edu>
2020-07-27 09:32:43 -0700

    second v

--- src/index.js
+++ src/index.js
@@ -15,31 +15,38 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     function showImages(cards) {
+        // debugger
         let cardnames = cards.map(card => card.name)
     
         let cardprices = cards.map(card => card.card_prices)
         let cardstep1 = cards.map(card => card.card_images[0])
         let cardstep2 = cardstep1.map(element => element)
         // debugger
+        cardstep2.forEach(card => displayimage(card))
 
-        let cardimage = cardstep2.map(cardelement => cardelement.image_url)
-        cardimage.forEach(cardurl => displayimage(cardurl))
-        debugger
+        
+        // debugger
         // cardimage.forEach(cardurl => fetchImg(cardurl))
     }
 
-    function displayimage(cardurl) {
+    function displayimage(card) {
         // debugger
+        // let cardimage = cardstep2.map(cardelement => cardelement.image_url)
+        // card.card_images.forEach(image => { 
         let img = document.createElement("img")
-        img.src = cardurl
+        img.src = card.image_url
         imgdiv.append(img)
+        // })
         
-        img.addEventListener("click", () => clearDOM(cardurl))
+        
+        
+        img.addEventListener("click", () => clearDOM(card))
     }
     
     function clearDOM (cardurl) {
         // imgdiv.parentNode.removeChild
         console.log(cardurl)
+        debugger
         // debugger
         // let oneimg = document.createElement("img")
         // oneimg.src = cardurl
@@ -51,7 +58,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     function fetchImg(cardurl) {
         console.log(cardurl)
-        debugger
+        // debugger
     }
 
 
